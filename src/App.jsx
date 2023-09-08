018107fb0d2e1c7e560e609559bed428295e3753
kang <breaker0105@nate.com>
2023-09-08 16:14:31 +0900

    Modal, Select

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import './App.css';
 import Button from './components/Button';
 import Input from './components/Input';
 import styled from 'styled-components';
+import Modal from './components/Modal';
+import Select from './components/Select';
 
 function App() {
   return (
     <Body>
       <Button />
       <Input />
+      <Modal />
+      <Select />
     </Body>
   );
 }
