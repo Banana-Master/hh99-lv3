2b29858cf719416d5f93629fae6a99c689353135
Banana-Master <tjdgh0313@gmail.com>
2023-09-08 18:13:03 +0900

    완성

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,66 +1,91 @@
-import React, { useEffect, useRef, useState } from 'react';
-import styled from 'styled-components';
+import React, { useState } from "react";
+import styled from "styled-components";
+import { BsArrowDownShort } from "react-icons/bs";
 
 function Select() {
-  const languageList = ['리액트', '자바', '스프링', '리액트네이티브'];
-  const [language, setLanguage] = useState('리액트');
+  const languageList = ["리액트", "자바", "스프링", "리액트네이티브"];
+  const [language, setLanguage] = useState(languageList[0]);
   const [showList, setShowList] = useState(false);
 
-  const toggleShowList = () => setShowList(!showList);
+  const [language2, setLanguage2] = useState(languageList[0]);
+  const [showList2, setShowList2] = useState(false);
+
+  const toggleShowList = () => {
+    setShowList(!showList);
+    setShowList2(false);
+  };
   const liClickHandler = (index) => {
     setLanguage(languageList[index]);
     toggleShowList();
   };
 
-  const selectWrapRef = useRef();
-  console.log('123', selectWrapRef.current)
-  useEffect(() => {
-    const clickListOutside = (e) => {
-      if (selectWrapRef.current && !selectWrapRef.current.contains(e.target)) {
-        toggleShowList();
-      }
-    };
-    document.addEventListener('mousedown', clickListOutside);
-    return () => {
-      document.removeEventListener('mousedown', clickListOutside);
-    };
-  }, []);
+  const toggleShowList2 = () => {
+    setShowList2(!showList2);
+    setShowList(false);
+  };
+  const liClickHandler2 = (index) => {
+    setLanguage2(languageList[index]);
+    toggleShowList2();
+    setShowList(false);
+  };
 
   return (
     <>
       <Wrap>
-      <h1>Select</h1>
-        <SelectButton
-          onClick={toggleShowList}
-        >
-          {language}
-          
-        </SelectButton>
-        {
-          showList &&
-          <div
-            ref={selectWrapRef}
-          >
-            <LanguageUl>
-            {
-              languageList.map((item, index) => {
-                return (
-                  <LanguageLi
-                    key={index}
-                    onClick={() => liClickHandler(index)}
-                  >
-                    { item }
-                  </LanguageLi>
-                )
-              })
-            }
-            </LanguageUl>
+        <h1>Select</h1>
+        <FlexBox>
+          <div>
+            <SelectButton onClick={toggleShowList}>
+              {language} <BsArrowDownShort />
+            </SelectButton>
+            {showList && (
+              <div style={{ position: "absolute" }}>
+                <LanguageUl>
+                  {languageList.map((item, index) => {
+                    return (
+                      <LanguageLi
+                        key={index}
+                        onClick={() => liClickHandler(index)}
+                      >
+                        {item}
+                      </LanguageLi>
+                    );
+                  })}
+                </LanguageUl>
+              </div>
+            )}
           </div>
-        }
+
+          <div>
+            <SelectButton onClick={toggleShowList2}>
+              {language2} <BsArrowDownShort />
+            </SelectButton>
+            {showList2 && (
+              <div>
+                <LanguageUl>
+                  {languageList.map((item, index) => {
+                    return (
+                      <LanguageLi
+                        key={index}
+                        onClick={() => liClickHandler2(index)}
+                      >
+                        {item}
+                      </LanguageLi>
+                    );
+                  })}
+                </LanguageUl>
+              </div>
+            )}
+          </div>
+        </FlexBox>
       </Wrap>
     </>
-  )
+  );
 }
+
+const FlexBox = styled.div`
+  display: flex;
+`;
 
 const Wrap = styled.div`
   border: 3px solid rgb(221, 221, 221);
@@ -80,6 +105,7 @@ const SelectButton = styled.button`
   border: 1px solid lightgrey;
   border-radius: 10px;
   cursor: pointer;
+  margin-left: 10px;
 `;
 
 const LanguageUl = styled.ul`
@@ -88,13 +114,13 @@ const LanguageUl = styled.ul`
   margin: 0;
   padding-left: 0;
   list-style: none;
-  position: absolute;
-  `;
+  margin-left: 10px;
+`;
 
 const LanguageLi = styled.li`
   height: 40px;
   display: flex;
-  justify-content: center;
+  padding-left: 20px;
   align-items: center;
   background-color: white;
   border-left: 1px solid lightgrey;
@@ -107,12 +133,14 @@ const LanguageLi = styled.li`
   }
   &:first-child {
     border-top: 1px solid lightgrey;
-    border-radius: 5px;
+    border-top-left-radius: 5px;
+    border-top-right-radius: 5px;
   }
   &:last-child {
     border-bottom: 1px solid lightgrey;
-    border-radius: 5px;
+    border-bottom-left-radius: 5px;
+    border-bottom-right-radius: 5px;
   }
 `;
 
-export default Select
+export default Select;
